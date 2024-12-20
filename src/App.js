f3f50281181f51ce336470a43bf83e036d6ab3e4
Rafael Zipão Silva <zipao.silva@gmail.com>
2024-12-20 20:10:21 -0300

    aula3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import './App.css';
 import './componentes/Header/index';
 import Header from './componentes/Header/index';
+import styled from 'styled-components';
+
+const AppContainter = styled.div`
+	width: 100vw;
+	height: 100vh;
+	background-image: linear-gradient(90deg, #002f52 35%, #326589);
+
+`
 
 function App() {
   return (
-    <div className='App'>
+    <AppContainter>
       <Header />
-    </div>  
+    </AppContainter>  
   );
 }
 
